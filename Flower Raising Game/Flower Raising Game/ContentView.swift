c66f37557b57a1da581b37268c06d88710ebf9fc
kenshin0413 <kenshin1234569@gmail.com>
2026-05-13 17:01:51 +0900

    初期

---
+++ Flower Raising Game/Flower Raising Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flower Raising Game
+//
+//  Created by miyamotokenshin on R 8/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flower Raising Game/Flower Raising Game/Flower_Raising_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flower_Raising_GameApp.swift
+//  Flower Raising Game
+//
+//  Created by miyamotokenshin on R 8/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Flower_Raising_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
